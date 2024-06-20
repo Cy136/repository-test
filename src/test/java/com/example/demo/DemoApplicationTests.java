83103d5b996dd0f224e1acf4dcfdeb410cab71a4
chenxinglong <15090139905@163.com>
2024-06-20 22:51:55 +0800

    github远程仓库提交测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo02;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo02ApplicationTests {
+class DemoApplicationTests {
 
     @Test
     void contextLoads() {
